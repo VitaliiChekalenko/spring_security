514fb17c2a8ab34eaa422dcf2dcdb2ab91aadb46
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-14 18:24:38 +0200

    UD.SACO Spring Security - Authorization l.92

--- src/main/java/com/chekalenkovitalii/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/chekalenkovitalii/spring/security/configuration/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.chekalenkovitalii.spring.security.configuration;
 
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -17,5 +18,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(userBuilder.username("vitalii").password("1111").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").password("1111").roles("HR"))
                 .withUser(userBuilder.username("alex").password("1111").roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
--- src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
+++ src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="securuty" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 
 <html>
@@ -5,13 +6,20 @@
 
 <h3> Information for all employees</h3>
 <br><br>
+
+<securuty:authorize access="hasRole('HR')">
 <input type="button" value="Salary"
             onclick="window.location.href = 'hr_info'">
 Only fo HR staff
+</securuty:authorize>
 <br><br>
+
+<securuty:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance"
        onclick="window.location.href = 'manager_info'">
+
 Only fo Managers
+</securuty:authorize>
 <br><br>
 </body>
 

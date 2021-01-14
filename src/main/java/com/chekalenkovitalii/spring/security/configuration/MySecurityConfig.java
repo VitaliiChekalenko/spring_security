9c50f4f80873d3f46f6e304fcd7335849c08d313
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-15 00:52:00 +0200

    UD.SACO Spring Security - Password in DB l.93

--- src/main/java/com/chekalenkovitalii/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/chekalenkovitalii/spring/security/configuration/MySecurityConfig.java
@@ -1,23 +1,31 @@
 package com.chekalenkovitalii.spring.security.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("vitalii").password("1111").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("1111").roles("HR"))
-                .withUser(userBuilder.username("alex").password("1111").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource((dataSource));
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("vitalii").password("1111").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("1111").roles("HR"))
+//                .withUser(userBuilder.username("alex").password("1111").roles("MANAGER", "HR"));
     }
 
     @Override

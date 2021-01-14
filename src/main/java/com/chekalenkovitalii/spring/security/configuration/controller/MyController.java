68341eab8da34edd15e42609347b0a5c9d57f805
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-14 18:03:54 +0200

    UD.SACO Spring Security - Authentication l.91

---
+++ src/main/java/com/chekalenkovitalii/spring/security/configuration/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.chekalenkovitalii.spring.security.configuration.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String getInfoForAllEmps() {
+        return "view-for-all-employees";
+    }
+
+    @GetMapping("/hr_info")
+    public String getInfoOnlyForHR() {
+        return "view-for-hr";
+    }
+
+    @GetMapping("/manager_info")
+    public String getInfoOnlyForManagers() {
+        return "view-for-managers";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<h3> Information for all employees</h3>
+<br><br>
+<input type="button" value="Salary"
+            onclick="window.location.href = 'hr_info'">
+Only fo HR staff
+<br><br>
+<input type="button" value="Performance"
+       onclick="window.location.href = 'manager_info'">
+Only fo Managers
+<br><br>
+</body>
+
+</html>
